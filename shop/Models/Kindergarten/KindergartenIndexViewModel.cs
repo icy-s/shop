34d4d3ecc154cf329fc3112f1d2409556ac77183
icy-s <evgeny.tailov@gmail.com>
2025-09-16 18:38:53 +0300

    Kindergarten view models

---
+++ shop/Models/Kindergarten/KindergartenIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace shop.Models.Kindergarten
+{
+    public class KindergartenIndexViewModel
+    {
+        public Guid id { get; set; }
+        public string? GroupName { get; set; }
+        public int ChildrenCount { get; set; }
+        public string? KindergartenName { get; set; }
+        public string? TeacherName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
