7437ecfe399a609bc02ffc30e632ef454f8518ee
icy-s <evgeny.tailov@gmail.com>
2025-09-16 18:44:46 +0300

    Kindergarten services

---
+++ shop.Core/ServiceInterface/IKindergartenServices.cs
@@ -0,0 +1,13 @@
+﻿using shop.Core.Domain;
+using shop.Core.Dto;
+
+namespace shop.Core.ServiceInterface
+{
+    public interface  IKindergartenServices
+    {
+        Task<Kindergarten> Create(KindergartenDto dto);
+        Task<Kindergarten> DetailAsync(Guid id);
+        Task<Kindergarten> Delete(Guid id);
+        Task<Kindergarten> Update(KindergartenDto dto);
+    }
+}
