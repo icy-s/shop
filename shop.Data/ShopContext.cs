b4eb0299a883714f3f7df6f03cc75126b3475423
icy-s <evgeny.tailov@gmail.com>
2025-09-16 18:57:10 +0300

    shopcontext changed for kindergarten

--- shop.Data/ShopContext.cs
+++ shop.Data/ShopContext.cs
@@ -12,7 +12,7 @@ namespace shop.Data
     {
         public ShopContext(DbContextOptions<ShopContext> options)
         : base(options){ }
-        public DbSet<Spaceship> Spaceships { get; set; }
+        public DbSet<Kindergarten> Kindergarten { get; set; }
 
         public DbSet<FileToApi> FileToApis { get; set; }
     }
