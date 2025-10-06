78add8bce358f154c8d955016588d4c88ec54f85
icy-s <evgeny.tailov@gmail.com>
2025-10-06 10:08:10 +0300

    fixed app code, still cursed

--- TripsTrapsTrull/App.xaml.cs
+++ TripsTrapsTrull/App.xaml.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.Maui.Controls;
-
-namespace SymbolGame
+﻿namespace SymbolGame
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
-            MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }   
     }
 }
