31821272bee11f9a0456783ebafe8323216099fd
icy-s <evgeny.tailov@gmail.com>
2025-10-21 17:20:40 +0300

    test push

--- shop.ApplicationServices/Services/WeatherForecastServices.cs
+++ shop.ApplicationServices/Services/WeatherForecastServices.cs
@@ -46,7 +46,7 @@ namespace shop.ApplicationServices.Services
 
         public async Task<AccuLocationWeatherResultDto> AccuWeatherResultWebClient(AccuLocationWeatherResultDto dto)
         {
-            string accuApiKey = "zpka_0c86f3fafa9147e58813fa06b647f221_9b9fd9d9";
+            string accuApiKey = "";
             string url = $"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={accuApiKey}&q={dto.CityName}";
 
             using (WebClient client = new WebClient())
